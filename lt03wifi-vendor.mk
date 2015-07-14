de10767a4808a0081c6bf3898cc628e8b9dfe128
Christopher N. Hesse <raymanfx@gmail.com>
2015-07-14 11:40:23 +0200

    lt03wifi: Remove deprecated include rule

--- lt03wifi-vendor.mk
+++ lt03wifi-vendor.mk
@@ -16,5 +16,3 @@
 
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS := vendor/samsung/lt03wifi/overlay
-
-$(call inherit-product, vendor/samsung/lt03wifi/lt03wifi-vendor-blobs.mk)
